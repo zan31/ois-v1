e93409393ebd75953636a777c2a3470d0131c077
neZan31 <zan.hrastnik3110@gmail.com>
2023-02-25 19:05:13 +0100

    izračun opravljen

--- Gravitacija.java
+++ Gravitacija.java
@@ -2,17 +2,17 @@ import java.util.*;
 
 class Gravitacija {
     public static void main(String args[]) {
-        /*
-         * Scanner sc = new Scanner(System.in);
-         * 
-         * final double C = 6.674e-11;
-         * final double M = 5.972e24;
-         * final double r = 6.371e6;
-         * double v = sc.nextDouble(); // visina
-         * 
-         * double a = (C * M) / ((r + v) * (r + v));
-         */
+    	System.out.println("OIS je zakonis");
+        
+         Scanner sc = new Scanner(System.in);
+          
+         final double C = 6.674e-11;
+         final double M = 5.972e24;
+         final double r = 6.371e6;
+         double v = sc.nextDouble(); // visina
+          
+         double a = (C * M) / ((r + v) * (r + v));
+         
 
-        System.out.println("OIS je zakonis");
     }
 }
